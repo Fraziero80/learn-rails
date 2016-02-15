a8aba5e856d2f4a68e1c41108cb276883e8590d4
Romar Frazier <romarfrazier@yahoo.com>
2016-02-14 16:28:27 -0800

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,16 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_api_key
+  }
+  # Action Mailer config
+  config.action_mailer.default.url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.raise_delivery_errors = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
